f3c02d4555eaf00cf0304d8c3e373c9621f07337
Rishabh Singh <33364895+Rishabh1839@users.noreply.github.com>
2019-03-05 20:36:09 -0700

    Add files via upload

--- ARIEL.py
+++ ARIEL.py
@@ -21,166 +21,50 @@ import csv
 import tensorflow as tf
 import os
 
+def engineCore():
 
-#User value for data import
-def userDataImport():
-    print("Enter the filepath for the CSV to import: ")
-    print("")
-    userData = input(" Filepath ")
+  print("Eneter filepath for 2 CSV's.")
+  print("")
+  csvFile1 = input("File Path1 : ")
+  print("")
+  csvFile2 = input("File Path")
 
+  #Open Both CSV
+  Col1 = "ColumnName1"
+  Col2 = "ColumnName2"
+  Col3 = "ColumnName3"
+  mydictionary = {Col1: [], Col2: [], Col3: []}
 
-#Import data as CSV and read columns
-def dataLoad(userData):
-    filenames = [userData]
-    record_defaults = [tf.float32] * 8 
-    dataset = tf.contrib.data.CsvDataset(filenames, record_defaults)
-
-
-def datasetSet(dataset):
-dataset1 = tf.data.Dataset.from_tensor_slices(tf.random_uniform([4, 10]))
-print(dataset1.output_types)  # ==> "tf.float32"
-print(dataset1.output_shapes)  # ==> "(10,)"
-
-dataset2 = tf.data.Dataset.from_tensor_slices(
-   (tf.random_uniform([4]),
-    tf.random_uniform([4, 100], maxval=100, dtype=tf.int32)))
-print(dataset2.output_types)  # ==> "(tf.float32, tf.int32)"
-print(dataset2.output_shapes)  # ==> "((), (100,))"
-
-dataset3 = tf.data.Dataset.zip((dataset1, dataset2))
-print(dataset3.output_types)  # ==> (tf.float32, (tf.float32, tf.int32))
-print(dataset3.output_shapes)  # ==> "(10, ((), (100,)))"
-
-#Create iterator
-dataset = tf.data.Dataset.range(100)
-iterator = dataset.make_one_shot_iterator()
-next_element = iterator.get_next()
-
-for i in range(100):
-  value = sess.run(next_element)
-  assert i == value
-
-max_value = tf.placeholder(tf.int64, shape=[])
-dataset = tf.data.Dataset.range(max_value)
-iterator = dataset.make_initializable_iterator()
-next_element = iterator.get_next()
-
-# Initialize an iterator over a dataset with 10 elements.
-sess.run(iterator.initializer, feed_dict={max_value: 10})
-for i in range(10):
-  value = sess.run(next_element)
-  assert i == value
-
-# Initialize the same iterator over a dataset with 100 elements.
-sess.run(iterator.initializer, feed_dict={max_value: 100})
-for i in range(100):
-  value = sess.run(next_element)
-  assert i == value
+  csv.reader(open(csvFile1, "rb"))
+  csv.reader(open(csvFile2, "rb"))
+  for row in csvFile:
+    mydictionary[Col1].append(row[0])
+    mydictionary[Col2].append(row[1])
+    mydictionary[Col3].append(row[2])
 
 
-#Everything below is tensorboard
-def variable_summaries(var):
-  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-  with tf.name_scope('summaries'):
-    mean = tf.reduce_mean(var)
-    tf.summary.scalar('mean', mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-    tf.summary.scalar('stddev', stddev)
-    tf.summary.scalar('max', tf.reduce_max(var))
-    tf.summary.scalar('min', tf.reduce_min(var))
-    tf.summary.histogram('histogram', var)
+  filename_queue = tf.train.string_input_producer([csvFile1, csvFile2])
 
-def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
-  """Reusable code for making a simple neural net layer.
+  reader = tf.TextLineReader()
+  key, value = reader.read(filename_queue)
 
-  It does a matrix multiply, bias add, and then uses relu to nonlinearize.
-  It also sets up name scoping so that the resultant graph is easy to read,
-  and adds a number of summary ops.
-  """
-  # Adding a name scope ensures logical grouping of the layers in the graph.
-  with tf.name_scope(layer_name):
-    # This Variable will hold the state of the weights for the layer
-    with tf.name_scope('weights'):
-      weights = weight_variable([input_dim, output_dim])
-      variable_summaries(weights)
-    with tf.name_scope('biases'):
-      biases = bias_variable([output_dim])
-      variable_summaries(biases)
-    with tf.name_scope('Wx_plus_b'):
-      preactivate = tf.matmul(input_tensor, weights) + biases
-      tf.summary.histogram('pre_activations', preactivate)
-    activations = act(preactivate, name='activation')
-    tf.summary.histogram('activations', activations)
-    return activations
+  # Default values, in case of empty columns. Also specifies the type of the
+  # decoded result.
+  record_defaults = [[1], [1], [1], [1], [1]]
+  col1, col2, col3, col4, col5 = tf.decode_csv(
+    value, record_defaults=record_defaults)
+  features = tf.stack([col1, col2, col3, col4])
 
-hidden1 = nn_layer(x, 784, 500, 'layer1')
+  with tf.Session() as sess:
+    # Start populating the filename queue.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
 
-with tf.name_scope('dropout'):
-  keep_prob = tf.placeholder(tf.float32)
-  tf.summary.scalar('dropout_keep_probability', keep_prob)
-  dropped = tf.nn.dropout(hidden1, keep_prob)
+    for i in range(1200):
+      # Retrieve a single instance:
+      example, label = sess.run([features, col5])
 
-# Do not apply softmax activation yet, see below.
-y = nn_layer(dropped, 500, 10, 'layer2', act=tf.identity)
+    coord.request_stop()
+    coord.join(threads)
+engineCore()
 
-with tf.name_scope('cross_entropy'):
-  # The raw formulation of cross-entropy,
-  #
-  # tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.softmax(y)),
-  #                               reduction_indices=[1]))
-  #
-  # can be numerically unstable.
-  #
-  # So here we use tf.losses.sparse_softmax_cross_entropy on the
-  # raw logit outputs of the nn_layer above.
-  with tf.name_scope('total'):
-    cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
-tf.summary.scalar('cross_entropy', cross_entropy)
-
-with tf.name_scope('train'):
-  train_step = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(
-      cross_entropy)
-
-with tf.name_scope('accuracy'):
-  with tf.name_scope('correct_prediction'):
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  with tf.name_scope('accuracy'):
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar('accuracy', accuracy)
-
-# Merge all the summaries and write them out to /tmp/mnist_logs (by default)
-merged = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/train',
-                                      sess.graph)
-test_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/test')
-tf.global_variables_initializer().run()
-
-#Training the data Recieved,
-# Train the model, and also write summaries.
-# Every 10th step, measure test-set accuracy, and write test summaries
-# All other steps, run train_step on training data, & add training summaries
-
-def feed_dict(train):
-  """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
-  if train or FLAGS.fake_data:
-    xs, ys = mnist.train.next_batch(100, fake_data=FLAGS.fake_data)
-    k = FLAGS.dropout
-  else:
-    xs, ys = mnist.test.images, mnist.test.labels
-    k = 1.0
-  return {x: xs, y_: ys, keep_prob: k}
-
-for i in range(FLAGS.max_steps):
-  if i % 10 == 0:  # Record summaries and test-set accuracy
-    summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(False))
-    test_writer.add_summary(summary, i)
-    print('Accuracy at step %s: %s' % (i, acc))
-  else:  # Record train set summaries, and train
-    summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
-    train_writer.add_summary(summary, i)
-
-#Running the entire application
-def runARIEL():
-  userDataImport()
-  
