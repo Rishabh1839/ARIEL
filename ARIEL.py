db15cc3c998b6da021b7e9e9ffa52c2a4af656e5
Rishabh1839 <rishabh_singh39@yahoo.com>
2019-04-04 20:01:17 -0700

    Adding ARIEL 1.0.0.0

--- ARIEL.py
+++ ARIEL.py
@@ -1,6 +1,6 @@
 # Ariel Core engine
 
-# ##################IMPORTS################################
+##################IMPORTS################################
 import os
 import tensorflow as tf
 import tensorboard
@@ -8,88 +8,32 @@ import csv
 import numpy as np
 from tensorflow import keras
 # using pandas and sklearn for testing and training our data by splitting the data
-import pandas as pd
-# splits data into 2 parts
-from sklearn.model_selection import train_test_split
+def csvImport():
+  print("ARIEL Start")
+  print("DEBUG: Starting User Input")
+  csvPath = input("Please supply the path to the CSV: ")
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-filename = dir_path + "training.csv"
+  dataset = tf.contrib.data.make_csv_dataset(csvPath, batch_size=32)
+  iter = dataset.make_one_shot_iterator()
+  next = iter.get_next()
+  print(next)
+  
+  inputs, labels = next['country'], next['year']
 
-features = tf.placeholder(tf.int32, shape=[3], name='features')
-country = tf.placeholder(tf.string, name='country')
-indicator = tf.placeholder(tf.string, name='indicator')
-total = tf.reduce_sum(features, name='total')
+  with tf.Session() as sess:
+    sess.run([inputs, labels])
 
-printerop = tf.print(total, [country, features, indicator, total], name='printerop')
+  ########################Creating an Initialiuzable Iterator##################################
+  #This will update the data at runtime making tesnorflow come alive.
+  def iterator(dataset):
+    x = tf.placeholder(tf.float32, shape=[None,2])
+    dataset = tf.data.Dataset.from_sparse_tensor_slices(x)
+    
+    data = np.random.sample((200, 2))
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    with open(filename) as inf:
-        # skip the header
-        next(inf)
-        for line in inf:
-            # reading our data using python into our features
-            country_name, indicator_name = line.strip(), line.split(", ")
-            total = sess.run(printerop, feed_dict={features: [country, indicator], country: country_name})
-            print(country_name, indicator_name)
+    iter = dataset.make_initializable_iterator()
+    el = iter.get_next()
 
-
-def create_file_reader(filename_queue):
-    reader = tf.TextLineReader(skip_header_line=1)
-    _, csv_row = reader.read(filename_queue)
-    record_defaults = [[""], [""], [0], [0], [0], [0]]
-    country_name, country_code, indicator_name = tf.decode_csv(csv_row, record_defaults=record_defaults)
-    features = tf.pack([country_name, country_code, indicator_name])
-    return features, country
-
-
-filename_queue = tf.train.string_input_producer(filename, num_epocs=1, shuffle=False)
-example, country, = create_file_reader(filename_queue)
-
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    coordinate = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coordinate=coordinate)
-    while True:
-        try:
-            example_data, country_name = sess.run([example, country])
-            print(example_data, country_name)
-        except tf.errors.OutOfRangeError:
-            break
-
-# def fileImport():
-# print("Enter file path for the unstructured data.")
-# userData = input("")
-# print(tf.__version__)
-
-# Reading the CSV
-# filenames = ["/home/hunt3r/Desktop/USData1.csv"]
-# record_defaults = [[0.0]] * 8
-# dataset = tf.data.experimental.CsvDataset(filenames, record_defaults)
-
-
-# dataset_url = ' '
-# data = pd.read_csv(dataset_url)
-# print(data.head())
-
-# dataset_url = ' '
-# data = pd.read_csv('training.csv')
-# print(data.head())
-
-
-# y = data.temp
-# x = data.drop('temp', axis=1)
-
-# X_train, X_test, Y_train, X_test = train_test_split(x, y, test_size=0.2)
-# print("\nX_train:\n")
-# print(X_train.head())
-# print(X_train.shape())
-
-# print("\nX_test:\n")
-# print(X_test.head())
-# print(X_test.shape)
-# #############IF I NEED ANYTHING ELSE, ILL ADD IT#########3
-
-# TO DO:
-# BELOW IS THE FILE IMPORT. WE ARE IMPORTING ALL FILES
-# IN A FOLDER ====> /PATH/TO/FOLDER/*   <====ALL FILES
+    with tf.Session() as sess:
+      sess.run(iter.initializer, feed_dict=(x: data))
+      print(sess.run(el))
